d08faa51cec3606d241403bba12d722775aaf74f
dixonary <alex@dixonary.co.uk>
2016-11-19 14:24:13 +0000

    initial

--- source/Cargo.hx
+++ source/Cargo.hx
@@ -4,6 +4,7 @@ import adventure.*;
 
 class Cargo extends Room {
     override public function create() {
+        scaleFactor = 5;
         objects = [new Player(0,0)];
     }
 }
--- source/Player.hx
+++ source/Player.hx
@@ -4,4 +4,8 @@ import adventure.*;
 
 class Player extends Object {
 
+    public function new(x,y) {
+        super(x,y);
+        loadGraphic("assets/images/player.png", true,10,10);
+    }
 }
